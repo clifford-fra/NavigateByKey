5216a5cccd41a800967a5d0eb10356f8bee91fdf
Clifford <55443386+clifford-fra@users.noreply.github.com>
2021-01-28 11:11:54 +0100

    Update navigateByKeyController.js
    Updated controller

--- force-app/main/default/aura/navigateByKey/navigateByKeyController.js
+++ force-app/main/default/aura/navigateByKey/navigateByKeyController.js
@@ -1,18 +1,56 @@
 ({
 	doInit : function(component, event, helper) {   
         addEventListener('keydown', function(event){
-            if(component.get("v.disabled") === false) {
-                if (event.keyCode === 13) {
-                    var navigate = component.get('v.navigateFlow');
-                    
-                    try {
-                        navigate("NEXT"); 
-                    } catch (err) {
-                        console.log('Type Error Navigation from navigateByKey');
-                    }
-                    
+
+            if (component.get("v.disabled") === true)
+                return;
+
+            var navigate = component.get('v.navigateFlow');
+
+            try {
+                
+                if (component.get("v.useCode") === true) {
+
+                    // Using event.code by default
+                    if (component.get('v.nextKey') != null && event.code === component.get('v.nextKey'))
+                        navigate("NEXT");
+
+                    if (component.get('v.previousKey') != null && event.code === component.get('v.previousKey'))
+                        navigate("BACK"); 
+    
+                    if (component.get('v.finishKey') != null && event.code === component.get('v.finishKey'))
+                        navigate("FINISH");
+
+                } else if (component.get("v.useKey") === true) {
+
+                    // Using event.key
+                    if (component.get('v.nextKey') != null && event.key === component.get('v.nextKey'))
+                        navigate("NEXT");
+
+                    if (component.get('v.previousKey') != null && event.key === component.get('v.previousKey'))
+                        navigate("BACK"); 
+
+                    if (component.get('v.finishKey') != null && event.key === component.get('v.finishKey'))
+                        navigate("FINISH");
+
+                } else if (component.get("v.useKeyCode") === true) {
+
+                    // IE compatible mode
+                    if (component.get('v.nextKey') != null && event.keyCode === parseInt(component.get('v.nextKey')))
+                        navigate("NEXT");
+    
+                    if (component.get('v.previousKey') != null && event.keyCode === parseInt(component.get('v.previousKey')))
+                        navigate("BACK"); 
+    
+                    if (component.get('v.finishKey') != null && event.keyCode === parseInt(component.get('v.finishKey')))
+                        navigate("FINISH"); 
                 }
-            }
+
+
+            } catch (err) {
+                console.log('Navigation Error from navigateByKey');
+            }            
+
         });
     //Do something else in init
 },
